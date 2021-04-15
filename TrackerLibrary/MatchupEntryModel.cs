6f4469b70ec32c6d95a8f04f94b595da3c1cdb77
Mdbaker19 <matthew.d.baker20@gmail.com>
2021-04-14 19:39:35 -0500

    Added models for projet

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// XML comment 
+        /// Represents one team in the matchup.
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+        /// <summary>
+        /// Represents the score for the particular team.
+        /// </summary>
+        public double Score { get; set; }
+        /// <summary>
+        /// Represents the matchup that this team cam from as the winner
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+    }
+}
